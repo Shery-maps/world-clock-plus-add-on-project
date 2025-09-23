2261a571fbf3eeb52b0c2e1d0092af72695025e3
Shery-maps <sherymaps@gmail.com>
2025-09-23 18:29:30 +0100

    added js

---
+++ src/index.js
@@ -0,0 +1,49 @@
+function updateTime() {
+  let harareElement = document.querySelector("#harare");
+  if (harareElement) {
+    let harareDateElement = harareElement.querySelector(".date");
+    let harareTimeElement = harareElement.querySelector(".time");
+    let harareTime = moment().tz("Africa/Harare");
+
+    harareDateElement.innerHTML = harareTime.format("MMMM Do YYYY");
+    harareTimeElement.innerHTML = harareTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let sidneyElement = document.querySelector("#sidney");
+  if (sidneyElement) {
+    let sidneyDateElement = sidneyElement.querySelector(".date");
+    let sidneyTimeElement = sidneyElement.querySelector(".time");
+    let sidneyTime = moment().tz("Australia/Sidney");
+
+    sidneyDateElement.innerHTML = sidneyTime.format("MMMM Do YYYY");
+    sidneyTimeElement.innerHTML = sidneyTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let shanghaiElement = document.querySelector("#harare");
+  if (shanghaiElement) {
+    let shanghaiDateElement = shanghaiElement.querySelector(".date");
+    let shanghaiTimeElement = shanghaiElement.querySelector(".time");
+    let shanghaiTime = moment().tz("Asia/Shaghai");
+
+    shanghaiDateElement.innerHTML = shanghaiTime.format("MMMM Do YYYY");
+    shanghaiTimeElement.innerHTML = shanghaiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let lagosElement = document.querySelector("#lagos");
+  if (lagosElement) {
+    let lagosDateElement = lagosElement.querySelector(".date");
+    let lagosTimeElement = lagosElement.querySelector(".time");
+    let lagosTime = moment().tz("Africa/Lagos");
+
+    lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
+    lagosTimeElement.innerHTML = lagosTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
